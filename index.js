99a5db1314073f4e7762d034adf30db8e11cfdc3
Liz Daly <liz7daly@gmail.com>
2020-05-13 19:23:50 +0100

    Run native functions

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const TimerFramework = require('./js/performance/TimerFramework')
 const consoleResults = require('./js/helpers/console')
 const writeToCSV = require('./js/helpers/csv')
 
-const performanceCheck = new TimerFramework(Array.prototype.reverse, 10000, 200000)
-writeToCSV(performanceCheck)
+const performanceCheck = new TimerFramework(Array.prototype.sort, 50000, 1000000)
 consoleResults(performanceCheck)
+writeToCSV(performanceCheck)
