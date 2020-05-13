d99acbb183d0a6cc6da3b48190355b1707be842d
Liz Daly <liz7daly@gmail.com>
2020-05-13 22:07:57 +0100

    Update reverse to complete for an array of 3
    Currently needs refactoring

--- js/algorithms/reverse.js
+++ js/algorithms/reverse.js
@@ -1,6 +1,24 @@
 module.exports = function reverse(array) {
-  if (array.length === 1) {
-    return array
+  let newArray = array
+  for(let i = 0; i < array.length - 1; i++) {
+    // [newArray[i], newArray[i + 1]] = [newArray[i + 1], newArray[i]]
+    let tempFirst = newArray[i]
+    newArray[i] = newArray[i + 1]
+    newArray[i + 1] = tempFirst 
+    console.log(newArray)
   }
-  return [array[0], array[1]] = [array[1], array[0]]
+  for(let y = 0; y < array.length - 2; y++) {
+    // [newArray[i], newArray[i + 1]] = [newArray[i + 1], newArray[i]]
+    let newTempFirst = newArray[y]
+    newArray[y] = newArray[y + 1]
+    newArray[y + 1] = newTempFirst 
+    console.log(newArray)
+  }
+  return newArray
 }
+
+//for array of 3:
+// [1,2,3]
+// [2,1,3]
+// [2,3,1]
+// [3,2,1]
--- spec/reverse.spec.js
+++ spec/reverse.spec.js
@@ -8,4 +8,8 @@ describe('reverse',() => {
   it('reverses an array of 2', () => {
     expect(reverse([1,2])).toEqual([2,1])
   })
+
+  it('reverses an array of 3', () => {
+    expect(reverse([1,2,3])).toEqual([3,2,1])
+  })
 })
