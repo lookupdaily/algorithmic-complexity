4c9b8c81090cfc883d1a1852539e15583dd52160
Liz Daly <liz7daly@gmail.com>
2020-05-25 11:26:22 +0100

    Refactor findDuplicates further
    As referenced in previous commit, this refactor brings the search for duplicates
    into the one loop, removing the need for a second loop, and improving efficiency
    at scale.

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const consoleResults = require('./js/helpers/console')
 const writeToCSV = require('./js/helpers/csv')
 
 // const myReverse = require('./js/algorithms/reverse')
-// const alicesReverse = require('./js/algorithms/working/alicesReverse')
+// const aliceReverse = require('./js/algorithms/working/alicesReverse')
 // const myShuffle = require('./js/algorithms/shuffle')
 const findDuplicates = require('./js/algorithms/findDuplicates')
 // const builtInReverse = arr => { arr.reverse() }
 
-const performanceCheck = new TimerFramework(findDuplicates, 10000, 200000, 50)
+const performanceCheck = new TimerFramework(findDuplicates, 50000, 1000000, 50)
 consoleResults(performanceCheck)
 writeToCSV(performanceCheck, 'findDuplicates2')
--- js/algorithms/findDuplicates.js
+++ js/algorithms/findDuplicates.js
@@ -1,21 +1,20 @@
   module.exports = function findDuplicates(arr) {
-    let object = []
+    let object = {}
     let duplicates = []
 
     // create an object with a count for each time a value appears
     arr.forEach((value) => {
+      // if it doesn't exist create a key value pair
       if(!object[value]) {
        object[value] = 0
       }
+      // if it does exist push it into duplicates array (only first time)
+      if(object[value] == 1) {
+        duplicates.push(value)
+      }
+      // increase the count by one so that duplicates will only be pushed once
       object[value] += 1
     })
 
-    // loop through object and find values greater than 1
-    // this step could be removed to improve efficiency
-    for(var prop in object) {
-      if(object[prop] > 1) {
-        duplicates.push(prop)
-      }
-    }
     return duplicates
   }
