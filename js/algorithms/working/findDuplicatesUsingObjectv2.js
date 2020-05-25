1bb2eef6ef1d886fcfb2563d877cd3aea7833712
Liz Daly <liz7daly@gmail.com>
2020-05-25 11:56:24 +0100

    Revert back to previous findDuplicate alogorithm
    I had previously combined steps into one loop in a bit to improve efficiency,
    yet it seems to have slowed the algorithm down, whilst making for some spikes.
    
    Switching back to previous iteration for now.

--- js/algorithms/working/findDuplicatesUsingObjectv2.js
+++ js/algorithms/working/findDuplicatesUsingObjectv2.js
@@ -4,17 +4,17 @@
 
     // create an object with a count for each time a value appears
     arr.forEach((value) => {
+      // if it doesn't exist create a key value pair
       if(!object[value]) {
        object[value] = 0
       }
+      // if it does exist push it into duplicates array (only first time)
+      if(object[value] == 1) {
+        duplicates.push(value)
+      }
+      // increase the count by one so that duplicates will only be pushed once
       object[value] += 1
     })
 
-    // search object and find values greater than 1
-    for(var prop in object) {
-      if(object[prop] > 1) {
-        duplicates.push(prop)
-      }
-    }
     return duplicates
   }
